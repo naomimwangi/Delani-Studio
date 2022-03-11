78a4bbac665ab5147dc98a74c15ef88babaf6613
naomimwangi <naomimwangi7@gmail.com>
2022-03-11 23:56:26 +0300

    add script effects to what we do section

---
+++ script.js
@@ -0,0 +1,14 @@
+$('#design-img').click(function () {
+    $('#design-img').slideDown('1500').hide('1000');
+    $('#design').show('1500');
+});
+
+$('dev-img').click(function(){
+    $('#dev-img').slideDown('1500').hide('1000');
+    $('#dev').show('1500');
+});
+
+$('prod-img').click(function () {
+    $('#prod-img').slideDown('1500').hide('1000');
+    $('#prod').show('1500');
+});
