8c774ea661e09e42cf1905c9770102ec78e23ad8
naomimwangi <naomimwangi7@gmail.com>
2022-03-12 21:13:23 +0300

    add hover effect on portfolio

--- script.js
+++ script.js
@@ -3,13 +3,68 @@ $('.design-img').click(function () {
     $('#design').show('1500');
     
 });
+$('#design').click(function () {
+    $('#design').slideDown('1500').hide('1000');
+    $('.design-img').show('1500');
+
+});
 
 $('.dev-img').click(function(){
     $('.dev-img').slideDown('1500').hide('1000');
     $('#dev').show('1500');
+});
+$('#dev').click(function () {
+    $('#dev').slideDown('1500').hide('1000');
+    $('.dev-img').show('1500');
 });
 
 $('.prod-img').click(function () {
     $('.prod-img').slideDown('1500').hide('1000');
     $('#prod').show('1500');
 });
+$('#prod').click(function () {
+    $('#prod').slideDown('1500').hide('1000');
+    $('.prod-img').show('1500');
+});
+
+
+$("#project1").mouseover(function () {
+    $("#hover1").show();
+}).mouseout(function () {
+        $("#hover1").hide();
+    });
+$("#project2").mouseover(function () {
+    $("#hover2").show();
+}).mouseout(function () {
+    $("#hover2").hide();
+    });
+$("#project3").mouseover(function () {
+        $("#hover3").show();
+}).mouseout(function () {
+        $("#hover3").hide();
+    });
+$("#project4").mouseover(function () {
+        $("#hover4").show();
+}).mouseout(function () {
+        $("#hover4").hide();
+    });
+$("#project5").mouseover(function () {
+        $("#hover5").show();
+ }).mouseout(function () {
+        $("#hover5").hide();
+    });
+$("#project6").mouseover(function () {
+        $("#hover6").show();
+}).mouseout(function () {
+        $("#hover6").hide();
+    });
+$("#project7").mouseover(function () {
+        $("#hover7").show();
+}).mouseout(function () {
+        $("#hover7").hide();
+    });
+$("#project8").mouseover(function () {
+        $("#hover8").show();
+}).mouseout(function () {
+        $("#hover8").hide();
+    });
